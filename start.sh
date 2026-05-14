3753aef57542d130efb97b75c8e5bf015ff996e2
Sahil Thakur <151663130+Sahil-Thakur-1@users.noreply.github.com>
2026-05-14 20:09:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,32 @@
+# #!/bin/sh
+
+# echo "Waiting for DB..."
+# sleep 10
+
+# echo "Clearing cache..."
+# php artisan config:clear
+# php artisan cache:clear
+
+# echo "Running migrations..."
+# php artisan migrate --force
+
+# echo "Starting server..."
+# php artisan serve --host=0.0.0.0 --port=10000
 #!/bin/sh
 
-echo "Waiting for DB..."
-sleep 10
+echo "Waiting for services..."
+sleep 5
 
-echo "Clearing cache..."
+echo "Clearing old cache..."
 php artisan config:clear
 php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
 
-echo "Running migrations..."
-php artisan migrate --force
+echo "Caching config..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-echo "Starting server..."
+echo "Starting Laravel server..."
 php artisan serve --host=0.0.0.0 --port=10000
